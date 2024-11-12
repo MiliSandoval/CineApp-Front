5347dc389fbcc1e9d616dc9712e3c437ffe22919
Milagros Sandoval <milisandoval976@gmail.com>
2024-11-12 01:25:10 -0300

    Paginas consultar actores, butacas,promociones y comprobante

---
+++ js/login.js
@@ -0,0 +1,35 @@
+// Agregamos un listener al formulario para manejar el envío
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (!username || !password) {
+        alert("Por favor, completa todos los campos.");
+        return;
+    }
+
+    try {
+        const response = await fetch('https://localhost:7254/api/Cine/usuario', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                usuario: username,    
+                contraseña: password  
+            })
+        });
+        
+        if (response.ok) {
+            const data = await response.json();
+          
+            localStorage.setItem('userData', JSON.stringify(data));
+            window.location.href = 'main.html';
+        } else {
+            const errorData = await response.json();
+            alert(errorData.message || 'Datos incorrectos');
+        }
+    } catch (error) {
+        console.error("Error en la solicitud:", error);
+        alert("Datos incorrectos");
+    }
+});
